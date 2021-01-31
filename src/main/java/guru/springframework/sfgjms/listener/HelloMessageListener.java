3153aa7135ef02a588c74eac1d60a48aa7c1e7d1
kqiu10 <qiukan97@gmail.com>
2021-01-31 12:53:42 -0500

    added listener to queue

--- src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -7,13 +7,12 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Component;
 
 /**
  * Date: 1/31/21
  * Question Description
  */
-@Component
+//@Component
 public class HelloMessageListener {
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message) {
